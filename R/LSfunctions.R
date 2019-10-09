bb5bf8d816b70330590ece0547991294778a3040
DanielWilliamsS <dan@danmat.com>
2019-10-09 11:06:47 +0100

    Actually staged the file now

--- R/LSfunctions.R
+++ R/LSfunctions.R
@@ -1,1 +1,15 @@
 
+LS.r = function(formula, data){
+
+  ys = all.vars(formula)[1]
+  y=Prostate[,ys]
+  X = model.matrix(formula, data)
+
+  wLS =  solve(t(X) %*% X) %*% t(X) %*% y
+
+  return(list(Parameters = wLS))
+}
+
+LS.p = function(model){
+  model$df %*% model$Parameters
+}
